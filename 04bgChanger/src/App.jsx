22904ae8b288e98146c0024633dd45027f07ae99
shan2302 <shantanu230205@gmail.com>
2025-06-29 01:20:58 +0530

    Bg_color_changer

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function App() {
+  const [color, SetColor] = useState("olive");
+
+  return (
+    <>
+      <div
+        className="w-full h-screen duration-200 "
+        style={{ backgroundColor: color}}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
+            <button  onClick={()=> SetColor("Red")}className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor:"red"}}>Red</button>
+            <button onClick={()=> SetColor("Green")}className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor:"green"}}>Green</button>
+            <button onClick={()=> SetColor("Blue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{backgroundColor:"blue"}}>Blue</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ interviewQuestion.md
@@ -0,0 +1,6 @@
+when we use multiple times in adding or subtracting value and showing to UI in Hooks it will only update 1 time
+Answer :-
+if we want to use multiple times 
+setCounter((prevCounter)=>prevCounter+1)
+
+3:56:56
